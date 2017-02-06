529ccc07cdcbc82f818bcf0d35117f2b8aa927cb
Sam <Sameish@users.noreply.github.com>
2017-02-06 15:50:23 +0000

    Add files via upload

---
+++ redditScript.js
@@ -0,0 +1,44 @@
+function renderPosts(posts) {
+  var templateSource = getTemplate('post-template');
+  var template = Handlebars.compile(templateSource);
+  var html = template({ posts: posts });
+  $('.posts').html(html);
+}
+
+function handleResponse(response) {
+  var responsePosts = response.data.children;
+  var posts = [];
+  for (var i = 0; i < responsePosts.length; i++) {
+    posts[i] = responsePosts[i].data;
+  }
+posts.forEach((posts) => {
+posts.hasThumbnail = posts.thumbnail !== "self";
+});
+
+posts.forEach((posts) => {
+posts.hasSelfText = posts.selftext !== "";
+});
+  renderPosts(posts);
+}
+
+function getTemplate(name) {
+  return $('script#' + name).html();
+}
+
+function getPosts() {
+  var subreddit = $('#subreddit').val();
+  $.getJSON('https://www.reddit.com/r/' + subreddit + '/top.json?sort=top&t=day').done(handleResponse);
+}
+
+function getDadJokes() {
+  var subreddit = $('#subreddit').val();
+  $.getJSON('https://www.reddit.com/r/dadjokes/top.json?sort=top&t=day').done(handleResponse);
+}
+
+$(function () {
+  $('#subButton').on('click', getPosts);
+});
+
+$(function () {
+  $('#dadJokes').on('click', getDadJokes);
+});
