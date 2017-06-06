5f5c8a2cf49267072849a326f0d477b04ed8026d
Sam <sam.t.hawkes@gmail.com>
2017-06-06 13:59:57 +0100

    Adding file

---
+++ Project Euler/problem9.js
@@ -0,0 +1,3 @@
+//The answer is a = 200, b = 375, c = 425.
+//abc = 31,875,000
+//If 1000 is divisble exactly by the sum of the primitive primes, then the answer is the product of the primitive primes with the result of that division 
